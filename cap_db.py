9d50a21bc13c7015a37ede785dbc0ba415474960
Shane Clark <clark.shanec@gmail.com>
2020-04-22 11:29:00 -0400

    Protect against records with missing data. Update comments.

--- cap_db.py
+++ cap_db.py
@@ -40,23 +40,28 @@ with open(fname) as f :
 for f in dict_data['features'] :
      project = f['properties']
 
-     if project['NAME'] != project['PROJECTNAME'] : print(project['NAME'], project['PROJECTNAME'])
-     if project['PROJECTNAME'] is None or project['APPROVALDATE'] is None : continue
+     #if project['NAME'] != project['PROJECTNAME'] : print(project['NAME'], project['PROJECTNAME'])
+     if project['APPROVALDATE'] is None : continue
 
-     pname = project['PROJECTNAME']
+     if project['NAME'] is not None : pname = project['NAME']
+     elif project['PROJECTNAME'] is not None : pname = project['PROJECTNAME']
+     else :
+         print('Error in Record: ', project['TRACKINGNUMBER'])
+         continue
      grant_amount = project['GRANTAMOUNT']
      #parse approval year
      approve_date = project['APPROVALDATE']
      approve_yr = int(approve_date[:4])
-     #get retrofit names
+     #get retrofit names from json keys
      kl = list(project.keys())
-     rname = kl[11:] #list of strings
+     rname = kl[11:]
+     # get grant values
      if project['SMIP'] == -1 : grant = 0
      elif project['GARP'] == -1 : grant = 1
      else : grant = None
 
      for i in range(len(rname)) :
-          rname[i] = (rname[i],) # make it into a list of tuples for executemany argument
+          rname[i] = (rname[i],) # make rname into a list of tuples for executemany argument
 
      cur.execute('''INSERT OR IGNORE INTO Projects (pname, grant, grant_amount, approve_yr)
      VALUES (?, ?, ?, ?)''', (pname, grant, grant_amount, approve_yr))
@@ -66,17 +71,20 @@ for f in dict_data['features'] :
      stmt = 'INSERT OR IGNORE INTO Retrofits (rname) VALUES (?)'
      cur.executemany(stmt, rname)
 
-     rstr = kl[11:]
+     rstr = kl[11:] #get retrofit names as list of strings to loop through
      for i in range(len(rstr)) :
          if project[rstr[i]] > 0 :
              number = project[rstr[i]]
              cur.execute('SELECT id FROM Retrofits WHERE rname = ?', rname[i])
              retro_id = cur.fetchone()[0]
 
+             #create many-to-many join table
              cur.execute('''INSERT OR REPLACE INTO Joint (number, project_id, retro_id)
              VALUES (?, ?, ?)''', (number, project_id, retro_id))
          else : continue
-#I love my coffee
 
 conn.commit()
+
+
+
 cur.close()
